622887f8044cd14d7aa3fe83e3d82e2ac10856d2
Owen Mundy <omundy@gmail.com>
2018-05-02 08:27:11 -0400

    Switch back to no keys

--- extension/content.js
+++ extension/content.js
@@ -1,31 +1,10 @@
-console.log("Stop Slide Scrolling in Google Presentation loaded")
+
+
+console.log("Stop Slide Scrolling in Google Presentation loaded");
 
 /**
- * 	Listen for scroll, and if ALT is not pressed, prevent it
+ * 	Listen for scroll, prevent it
  */
 window.addEventListener("mousewheel", function(event) {
-	if (keys.alt != true)
-		event.stopPropagation();
+    event.stopPropagation();
 }, true);
-
-
-
-// object to track pressed keys
-var keys = {
-	alt: false,
-};
-
-/**
- * 	If keydown detected
- */
-document.onkeydown = function(event) {
-	if (event.keyCode == 18) keys.alt = true; // alt
-	//console.log(event.keyCode,keys);
-};
-/**
- * 	Reset keys that are no longer pressed
- */
-document.onkeyup = function(event) {
-	if (event.keyCode == 18) keys.alt = false;
-	//console.log(event.keyCode,keys);
-};
